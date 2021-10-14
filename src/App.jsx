9034c7b9208fcf309a4a4749a462e3bc2228b894
emeric237 <bestvemeric@gmail.com>
2021-10-14 09:42:02 -0400

    Working on contact section phone version

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,29 @@ import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import Topbar from "./components/topbar/Topbar";
 import Menu from "./components/menu/Menu";
+import { useMediaQuery } from "react-responsive";
 import "./app.scss";
 
 function App() {
+  const isMobileDevice = useMediaQuery({
+    query: "(min-device-width: 480px)",
+  });
+
+  const isTabletDevice = useMediaQuery({
+    query: "(min-device-width: 768px)",
+  });
+
+  const isLaptop = useMediaQuery({
+    query: "(min-device-width: 1024px)",
+  });
+
+  const isDesktop = useMediaQuery({
+    query: "(min-device-width: 1200px)",
+  });
+
+  const isBigScreen = useMediaQuery({
+    query: "(min-device-width: 1201px )",
+  });
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -15,6 +15,34 @@ export default function Contact() {
       style={{ backgroundImage: "url(/images/backgrounds/contact_blue.jpg)" }}
       id="contact"
     >
+      <div className="card section-lg bg-gradient-info shadow-lg border-0">
+        <div className="container">
+          <div className="p-2">
+            <div className="row">
+              <div className="col order-lg-2" lg="4">
+                <img
+                  src={"/images/man.png"}
+                  style={{ width: "200px" }}
+                  alt=""
+                  className="rounded-circle img-center img-fluid shadow shadow-lg--hover mb-4"
+                />
+              </div>
+              <div className="col col-8 order-lg-1">
+                <h2 className="text-white">Reach Out to me!</h2>
+                <p className="lead text-white mt-3">
+                  DISCUSS A PROJECT OR JUST WANT TO SAY HI? MY INBOX IS OPEN FOR
+                  ALL
+                </p>
+                <p className="text-white mt-3">hello</p>
+                <div className="my-3 icon-shape bg-gradient-white shadow rounded text-info">
+                  <i className="ni ni-pin-3 text-info mr-2" />
+                  {}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="left">
         <img src="images/shake.svg" alt="" />
       </div>
